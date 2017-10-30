d01ad27dd66b23cbc9d24d4018f67a419ecf3d7f
yangxuefeng <2368796034@qq.com>
2017-10-30 18:50:49 +0800

    45678

---
+++ demo01/src/main/java/bwei/com/demo01/MainActivity.java
@@ -0,0 +1,16 @@
+package bwei.com.demo01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+}
